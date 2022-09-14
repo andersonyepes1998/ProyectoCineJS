6b9a433589c37b6b39dd9a473e85ce225a5ef023
anderson <andersonyepesbedoya@gmail.com>
2022-09-13 21:34:32 -0500

    09-13-2022

--- CineMiercoles/src/controllers/controladores.js
+++ CineMiercoles/src/controllers/controladores.js
@@ -34,9 +34,9 @@ let peliculas=[
         nombre:"DC Liga De SúperMascotas",
         genero:"Animación",
         duracion:106,
-        poster:"https://firebasestorage.googleapis.com/v0/b/cineayb.appspot.com/o/pelicula1.jpg?alt=media&token=a830cd0c-7540-40ab-bec5-b2c21393a140",
+        poster:"https://firebasestorage.googleapis.com/v0/b/cineayb.appspot.com/o/SUPERMASCOTAS.jpg?alt=media&token=8a8efd92-9025-4050-a091-c612eb2a5067",
         sinopsis:"Cuando la Liga de la Justicia es capturada por Lex Luthor, el perro de Superman, Krypto, forma un equipo de mascotas de refugio a las que se les otorgan superpoderes: Un sabueso llamado Ace, que se vuelve superfuerte, un cerdo llamado PB, que puede crecer hasta alcanzar un tamaño gigante, una tortuga llamada Merton, que se vuelve superrápida, y una ardilla llamada Chip, adquiere poderes eléctricos.",
-        clasificacion:"general",
+        clasificacion:"publico en general",
         idioma: "En subtitulado",
         Director: "Jared Stern",
         Actores:["Kate McKinnon ", "Dwayne Johnson ", "Kevin Hart. "] 
@@ -45,9 +45,9 @@ let peliculas=[
         nombre:"Thor: Amor y Trueno",
         genero:"Aventura",
         duracion:119,
-        poster:"https://firebasestorage.googleapis.com/v0/b/cineayb.appspot.com/o/pelicula2.png?alt=media&token=b8b9ff9c-e78b-49e7-acc6-e88032983e87",
+        poster:"https://firebasestorage.googleapis.com/v0/b/cineayb.appspot.com/o/pelicula2.png?alt=media&token=009e1d50-9b5f-4edc-b661-44a7c2cb47d3",
         sinopsis:"La película encuentra a Thor (Chris Hemsworth) en un viaje diferente a todo lo que ha enfrentado: una búsqueda de paz interior. Pero su retiro es interrumpido por un asesino de la galaxia conocido como Gorr, el Carnicero de Dioses (Christian Bale), que busca la extinción de los dioses. Para combatir la amenaza, Thor solicita la ayuda del rey Valquiria (Tessa Thompson), Korg (Taika Waititi) y su ex novia Jane Foster (Natalie Portman), quien, para sorpresa de Thor, empuña inexplicablemente su martillo mágico, Mjolnir, como la Poderosa Thor. ",
-        clasificacion:"+12",
+        clasificacion:"mayores de 12 años",
         idioma: "En subtitulado",
         Director: "Anderson yepes ",
         Actores: "Chris Hemsworth, Natalie Portman, Taika Waititi"
@@ -56,20 +56,20 @@ let peliculas=[
         nombre:"El Teléfono Negro",
         genero:"Terror",
         duracion:107,
-        poster:"https://firebasestorage.googleapis.com/v0/b/cineayb.appspot.com/o/pelicula3.png?alt=media&token=a7b3972c-c154-470d-8d86-0d3df1df90d9",
+        poster:"https://firebasestorage.googleapis.com/v0/b/cineayb.appspot.com/o/pelicula3.png?alt=media&token=2d760080-2b76-42b7-aa20-eefd1cc22e34",
         sinopsis:"Finney Shaw es un niño tímido pero inteligente de 13 años que es secuestrado y atrapado en un sótano insonorizado por un asesino sádico. Cuando un teléfono desconectado en la pared comienza a sonar, Finney descubre que puede escuchar las voces de las víctimas anteriores del asesino. Y están decididos a asegurarse de que lo que les pasó a ellos no le pase a Finney.",
-        clasificacion:"+15",
+        clasificacion:"mayores de 15 años",
         idioma: "En subtitulado",
         Director: "Anderson yepes ",
         Actores: "Chris Hemsworth, Natalie Portman, Christian Bale"
     },
     {
-        nombre:"Elvis",
+        nombre:"La Chica Salvaje",
         genero:"Drama",
-        duracion:160,
-        poster:"https://firebasestorage.googleapis.com/v0/b/cineayb.appspot.com/o/plicula4.png?alt=media&token=10d1944d-e0ed-40d5-b4e4-14d9b31f1e0e",
-        sinopsis:"La película explora la vida y la música de Elvis Presley (Butler) a través del prisma de su complicada relación con el coronel Tom Parker (Hanks), su enigmático manager. La historia profundiza en la compleja dinámica que existía entre Presley y Parker que abarca más de 20 años, desde el ascenso de Presley a la fama hasta su estrellato sin precedentes, en el contexto de la revolución cultural y la pérdida de la inocencia en Estados Unidos. Y en el centro de ese periplo está Priscilla Presley (Olivia DeJonge), una de las personas más importantes e influyentes en la vida de Elvis.",
-        clasificacion:"+12",
+        duracion:125,
+        poster:"https://firebasestorage.googleapis.com/v0/b/cineayb.appspot.com/o/plicula4.png?alt=media&token=36fe6086-5797-4123-9d5a-1de5287ace59",
+        sinopsis:"Cuenta la historia de Kya, una niña abandonada que se crió hasta la edad adulta en los peligrosos pantanos de Carolina del Norte. Durante años, los rumores sobre la chica del pantano persiguieron a Barkley Cove, aislando a la aguda y resistente Kya de su comunidad. Atraída por dos jóvenes del pueblo, Kya se abre a un mundo nuevo y sorprendente",
+        clasificacion:"mayores de 12 años",
         idioma: "Es subtitulado",
         Director: "Valeria Moncada ",
         Actores: "Austin Butler, Tom Hanks, Dacre Montgomery"
@@ -80,7 +80,7 @@ let peliculas=[
         duracion:180,
         poster:"https://firebasestorage.googleapis.com/v0/b/cineayb.appspot.com/o/pelicula%205.png?alt=media&token=907c477c-ec30-46c2-9767-eaca36bf07c1",
         sinopsis:"secuela de la cinta de culto WYRMWOOD: ROAD TO DEAD, se lleva a cabo en un desierto australiano infestado de zombies donde el soldado Rhys quien ha dedicado su vida a rastrear y capturar supervivientes para el Cirujano General, se une a otros científicos locos, soldados malvados y héroes con la esperanza de encontrar una cura para el virus que amenaza a toda la humanidad.",
-        clasificacion:"+18",
+        clasificacion:"mayores de 18 años",
         idioma: "En subtitulado",
         Director: "Euclidez Perez ",
         Actores: "Oris Erhuero, Mark Strange, Martyn Ford"
@@ -89,9 +89,9 @@ let peliculas=[
         nombre:"El Perro Samurai: La Leyenda de Kakamucho",
         genero:"Animacion",
         duracion:180,
-        poster:"https://firebasestorage.googleapis.com/v0/b/cineayb.appspot.com/o/pelicula6.png?alt=media&token=441e8dc4-6cb1-45c8-b963-46f41dff2d4d",
+        poster:"https://firebasestorage.googleapis.com/v0/b/cineayb.appspot.com/o/pelicula6.png?alt=media&token=1a102d09-d604-4575-9abf-a9087a063e1b",
         sinopsis:"Hank, un sabueso con mala suerte, se encuentra en un pueblo lleno de gatos que necesitan un héroe que los defienda de un despiadado villano que quiere borrar a su aldea del mapa. Con la ayuda de un maestro que no quiere entrenarlo, nuestro desvalido héroe debe asumir el papel del samurái del pueblo y unirse a los aldeanos para salvar el día. El Perro Samurái: La leyenda de Kakamucho, próximamente en cines. Con las voces de Juanpa Zurita como Hank, Faisy como Ika Chu y Karla Díaz como Emiko.",
-        clasificacion:"general",
+        clasificacion:"publico en general",
         idioma: "Es subtitulado",
         Director: "Andres mercado",
         Actores: "Samuel L. Jackson, Michael Cera, Mel Brooks"
@@ -100,9 +100,9 @@ let peliculas=[
         nombre:"Tren Bala",
         genero:"Acción, Thriller",
         duracion:126 ,
-        poster:"https://firebasestorage.googleapis.com/v0/b/cineayb.appspot.com/o/pelicula7.jpg?alt=media&token=e9023645-6415-479d-89c0-f5230b625eab",
+        poster:"https://firebasestorage.googleapis.com/v0/b/cineayb.appspot.com/o/pelicula7.jpg?alt=media&token=57d4f1c0-0d03-4fda-8826-68b094ee59c6",
         sinopsis:"En Tren bala, Brad Pitt protagoniza la película en el papel de Ladybug, un asesino con poca suerte determinado a hacer su trabajo en paz después de que más de una de sus asignaciones se le han salido control. Sin embargo, el destino puede tener otros planes mientras que la última misión de Ladybug lo pone en un camino accidentado con adversarios mortales de diversas partes del planeta -todos conectados pero con objetivos en conflicto- en el tren más rápido del mundo…y tiene que descubrir como bajarse. Del director de Deadpool 2, David Leitch, el final del camino es sólo el comienzo de un viaje extremo sin parar a través de un Japón Moderno.",
-        clasificacion:"general",
+        clasificacion:"publico en general",
         idioma: "En subtitulado",
         Director: "Mathias yepes",
         Actores: "Brad Pitt, Joey King, David Leitch"
@@ -111,9 +111,9 @@ let peliculas=[
         nombre:"Todo en Todas Partes al Mismo Tiempo",
         genero:"Aventura",
         duracion:139,
-        poster:"https://firebasestorage.googleapis.com/v0/b/cineayb.appspot.com/o/pelicual8.jpg?alt=media&token=9209d2f9-19fc-4578-85a3-8df4daa50139",
+        poster:"https://firebasestorage.googleapis.com/v0/b/cineayb.appspot.com/o/TODO%20MISMO%20TIEMPO.png?alt=media&token=8c1e7c83-9fab-42cc-9a16-8eecc63c25cc",
         sinopsis:"Con su lavandería al borde del fracaso y su matrimonio con el debilucho esposo Waymond en las rocas, Evelyn Wang, con exceso de trabajo, lucha por hacer frente a todo, incluida una relación hecha jirones con su padre crítico Gong Gong y Joy, su hija. Y, como si enfrentarse a una lúgubre crisis de la mediana edad no fuera suficiente, Evelyn debe prepararse para una reunión desagradable con un burócrata impersonal: Deirdre, la auditora del IRS mal vestida.",
-        clasificacion:"+12",
+        clasificacion:"mayores de 12 años",
         idioma: "Es subtitulado",
         Director: "Johana Hernandez",
         Actores: "Michelle Yeoh, Stephanie Hsu, Dan Kwan"
@@ -124,7 +124,7 @@ let peliculas=[
         duracion:87,
         poster:"https://firebasestorage.googleapis.com/v0/b/cineayb.appspot.com/o/Pelicula%209.jpg?alt=media&token=739ff7d0-f079-4d42-b5f4-be1ba0c1c37e",
         sinopsis:"En el corazón de los años 70´s, un Gru de 12 años crece en los suburbios. Fan de los supervillanos Vicious 6, Gru decide que quiere ser tan malvado como ellos para unírseles. Con ayuda de sus secuaces, los Minions, construye su primera guarida, usa sus primeras armas y cumple sus primeras misiones.",
-        clasificacion:"En general",
+        clasificacion:"publico en general",
         idioma: "En subtitulado",
         Director: "Daniela gutierrez",
         Actores: "Julie Andrews, Russell Brand, Michelle Yeoh"
@@ -135,8 +135,30 @@ let peliculas=[
         duracion:90,
         poster:"https://firebasestorage.googleapis.com/v0/b/cineayb.appspot.com/o/Pelicula%2010.png?alt=media&token=82dc19e7-45d0-4a8d-a1e5-d4641d44c2b5",
         sinopsis:"Armando Pulido, un maestro de obra sencillo de buen corazón, trabajador, rebuscador y buen amigo, cansado de la grave crisis económica que vive en Colombia, decide irse a buscar el sueño americano a Nueva York, para esto intentan diversas formas de conseguir la visa americana hasta que lo logra de la manera menos pensada posible, al llegar a la gran manzana el hombre se ve enfrentado a rebuscársela con todo el ingenio. Al final logrará su sueño americano, pero le hará falta todo lo maravilloso que tenía en Colombia.",
-        clasificacion:"En general",
-        idioma: "Es",
+        clasificacion:"publico en general",
+        idioma: "Es Subtitulada",
+        Director: "Claudia Henao",
+        Actores: "Carlos Hurtado Beltrán, Harold Trompetero,  Bayardo Ardila"
+    },
+    {
+        nombre:"Vértigo",
+        genero:"Suspenso",
+        duracion:107,
+        poster:"https://firebasestorage.googleapis.com/v0/b/cineayb.appspot.com/o/VERTIGO.png?alt=media&token=8d6a2f23-eba1-45b5-8462-3b2c66b4756d",
+        sinopsis:"Para las mejores amigas Becky (Grace Caroline Currey) y Hunter (Virginia Gardner), la vida consiste en conquistar los miedos y superar los límites. Pero después de escalar 600 metros hasta la cima de una remota torre de radio abandonada, se encuentran atrapadas e incomunicadas. Ahora, las habilidades de escalada de Becky y Hunter serán puestas a prueba mientras luchan desesperadamente por sobrevivir.",
+        clasificacion:"mayores de 12 años",
+        idioma: "Es Subtitulada",
+        Director: "Claudia Henao",
+        Actores: "Carlos Hurtado Beltrán, Harold Trompetero,  Bayardo Ardila"
+    },
+    {
+        nombre:"Cacería de Brujas",
+        genero:"Terror, Thriller",
+        duracion:92,
+        poster:"https://firebasestorage.googleapis.com/v0/b/cineayb.appspot.com/o/CACERIAS%20DE%20BRUJAS.png?alt=media&token=dbf49d4c-0501-48d1-958e-d6ab7684b844",
+        sinopsis:"Cacería de Brujas está situada en un Estados Unidos moderno en el que las brujas son reales y la brujería es ilegal, una adolescente debe enfrentarse a sus propios demonios y prejuicios mientras ayuda a dos brujas a evitar a las fuerzas de la autoridad y a cruzar la frontera sur para refugiarse en México.",
+        clasificacion:"mayores de 12 años",
+        idioma: "Es Subtitulada",
         Director: "Claudia Henao",
         Actores: "Carlos Hurtado Beltrán, Harold Trompetero,  Bayardo Ardila"
     }
@@ -164,10 +186,11 @@ peliculas.forEach(function(pelicula){
     //1. Creamos una columna para cada variable.
 
     let columna=document.createElement('div');
-    columna.classList.add('col');
+    columna.classList.add('col',);
 
     let tarjeta=document.createElement('div');
     tarjeta.classList.add('card','h-100','shadow');
+
 
     //foto para cada pelicula
 
@@ -175,36 +198,47 @@ peliculas.forEach(function(pelicula){
     poster.classList.add('card-img-top');
     poster.src=pelicula.poster;
 
-    let nombrePelicula=document.createElement('h3');
-    nombrePelicula.classList.add('Card-title','text-center','fw-bold');
+    let nombrePelicula=document.createElement('h5');
+    nombrePelicula.classList.add('Card-title','fw-bold');
     nombrePelicula.textContent=pelicula.nombre;
 
-    let generoPelicula=document.createElement('h4');
+    let generoPelicula=document.createElement('p');
     generoPelicula.classList.add('card-text');
     generoPelicula.textContent="Genero: "+pelicula.genero;
 
-    let idioma = document.createElement('h6');
-    idioma.classList.add('fw-bold');
-    idioma.innerText=pelicula.idioma;
+    let idioma = document.createElement('p');
+    idioma.innerText= "Idioma: "+pelicula.idioma;
 
-    let director = document.createElement('h6');
+    let clasificacion = document.createElement('p');
+    clasificacion.classList.add('text');
+    clasificacion.innerText= "Recomendada para "+pelicula.clasificacion;
+
+    let duracion = document.createElement('p');
+    duracion.classList.add('text2');
+    duracion.innerText= pelicula.duracion+ " Min";
+
+    let director = document.createElement('h7');
     director.classList.add('fw-bold');
+    director.classList.add('d-none');
     director.innerText= "Director: " +pelicula.Director;
 
-    let actor = document.createElement('h6');
+    let actor = document.createElement('h7');
     actor.classList.add('fw-bold','text-center');
+    actor.classList.add('d-none');
     actor.innerText= "ACTORES: " +pelicula.Actores;
 
     let sinopsis = document.createElement('p');
-    //sinopsis.classList.add('d-none');
+    sinopsis.classList.add('d-none');
     sinopsis.textContent=pelicula.sinopsis;
     //padres e hijos
 
     tarjeta.appendChild(poster);
     tarjeta.appendChild(nombrePelicula);
     tarjeta.appendChild(generoPelicula);
-    tarjeta.appendChild(director);
     tarjeta.appendChild(idioma);
+    tarjeta.appendChild(clasificacion);
+    tarjeta.appendChild(duracion);
+    tarjeta.appendChild(director);
     tarjeta.appendChild(sinopsis);
     tarjeta.appendChild(actor);
 
@@ -303,7 +337,7 @@ let fila2=document.getElementById('fila2');
         poster.src=peliculita.poster;
 
         let nombPelicula=document.createElement('h3');
-        nombPelicula.classList.add('Card-title','text-center','fw-bold');
+        nombPelicula.classList.add('Card-title','fw-bold');
         nombPelicula.textContent=peliculita.nombre;
 
         
@@ -383,3 +417,4 @@ let fila2=document.getElementById('fila2');
     container.appendChild(rowjustify);
 
   }*/
+ 
